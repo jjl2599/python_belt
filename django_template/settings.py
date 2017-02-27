dc1c5c4679ce2d50a7bfbb8f1471f90cc2535fdc
Ubuntu <ubuntu@ip-172-31-25-240.us-west-2.compute.internal>
2017-02-27 05:02:06 +0000

    AWS Push

--- django_template/settings.py
+++ django_template/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zpye#+u@+jth6&4#nyu)#vwbhh%7630+2pprb6zrw-2)p^-w42'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.202.77.58']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
